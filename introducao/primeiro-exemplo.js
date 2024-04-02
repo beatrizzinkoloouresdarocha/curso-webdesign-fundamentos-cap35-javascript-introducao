b18c67f94640a8ba07fbc83606b3006f666b4a3d
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-04-02 10:13:30 -0300

    estou treinando pra colocar pra rodar o javascript com documentos.

--- introducao/primeiro-exemplo.js
+++ introducao/primeiro-exemplo.js
@@ -1,4 +1,12 @@
 console.log("Javascript funcionando 2222");
 
-let nome = "Daniel"
-console .log(10 +20)
+let nome = "Daniel";
+console.log(10 + 20);
+
+console.log("qualquer coisa");
+
+// uma única linha
+
+alert(nome); 
+
+let outraVariavel = "Daniel";
