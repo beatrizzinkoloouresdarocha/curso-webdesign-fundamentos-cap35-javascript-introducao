89812c2b06356d80735f433f1de44745f7e788c4
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-04-03 13:16:35 -0300

    consegui calcular minha imc pra saber qual maior e a menor .

--- funcoes/exercicio-imc.js
+++ funcoes/exercicio-imc.js
@@ -8,17 +8,32 @@ Obesidade Grau II 35 a 40 kg/m2
 Obesidade Grau III maior que 40 kg/m2
 */
 
-function calcularIMC(peso,altura){
-    if(peso === undefined || altura === undefined){
-        throw Error("need two parameters: weigth and height")
+function calcularIMC(peso, altura) {
+    if (peso === undefined || altura === undefined) {
+        throw Error("É necessário informar dois parâmetros: peso e altura");
     }
 
-    return peso / (altura * altura)
+    return peso / (altura * altura);
 }
 
-function classificaIMC(imc){
-    if(imc < 16.9){
-
-    }else if(imc > 17 && imc <18.4)
+function classificaIMC(imc) {
+    if (imc <= 16.9) {
+        return "Muito abaixo do peso";
+    } else if (imc >= 17 && imc <= 18.4) {
+        return "Abaixo do peso";
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        return "Peso normal";
+    } else if (imc >= 25 && imc <= 29.9) {
+        return "Acima do peso";
+    } else if (imc >= 30 && imc <= 34.9) {
+        return "Obesidade Grau I";
+    } else if (imc >= 35 && imc <= 40) {
+        return "Obesidade Grau II";
+    } else {
+        return "Obesidade Grau III";
+    }
 }
-let imc = calcularIMC(80,1.65)
+
+let imc = calcularIMC(80, 1.65);
+console.log("IMC:", imc);
+console.log("Classificação:", classificaIMC(imc));
