976b9edb82ed99ae627f52a5b584ded97f6d71ec
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-04-03 14:06:47 -0300

    funcao anonima callback consegui arrumar o erro concluido coloquei pra rodar e funcionou .

--- funcoes/callback.js
+++ funcoes/callback.js
@@ -1,4 +1,8 @@
-function teste() {
-    console.log("funcao teste")
+function teste(cb, n) {
+    console.log("funcao teste", (n + n));
+    cb();
 }
-teste()
+
+teste(function() {
+    console.log("funcao anonima callback");
+}, 5); // Passando um valor para n, por exemplo, 5
